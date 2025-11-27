abe8d9ed8fed3d57f7cde5749212f8a619b6fab5
syawalqi <galihmaulanasyawalqi@gmail.com>
2025-11-27 09:37:53 +0700

    membuat ContainerApp.kt/ interface containerapp/ override val repositoriSiswa

--- app/src/main/java/com/example/pertemuan9/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/pertemuan9/repositori/ContainerApp.kt
@@ -3,3 +3,10 @@ package com.example.pertemuan9.repositori
 interface ContainerApp {
     val repositoriSiswa : RepositoriSiswa
 }
+
+class ContainerDataApp(private val context: Context):
+    ContainerApp{
+    override val repositoriSiswa: RepositoriSiswa by lazy {
+        OfflineRepositoriSiswa(DatabaseSiswa.getDatabase(context).siswaDao())
+    }
+}
