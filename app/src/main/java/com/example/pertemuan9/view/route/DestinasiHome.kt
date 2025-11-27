80f2b4310b17ca91713ce1bc0370ce85e3c30ec6
syawalqi <galihmaulanasyawalqi@gmail.com>
2025-11-27 13:15:21 +0700

    konfigurasi

--- app/src/main/java/com/example/pertemuan9/view/route/DestinasiHome.kt
+++ app/src/main/java/com/example/pertemuan9/view/route/DestinasiHome.kt
@@ -1,4 +1,7 @@
 package com.example.pertemuan9.view.route
 
-object DestinasiHome {
+import com.example.pertemuan9.R
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
 }
