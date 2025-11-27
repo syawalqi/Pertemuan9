c77030a9295a0c80650194b0dc241f5bfe867dc4
syawalqi <galihmaulanasyawalqi@gmail.com>
2025-11-27 09:19:15 +0700

    mmebuat file interface/ konfigurasi

--- app/src/main/java/com/example/pertemuan9/room/SiswaDao.kt
+++ app/src/main/java/com/example/pertemuan9/room/SiswaDao.kt
@@ -1,4 +1,17 @@
 package com.example.pertemuan9.room
 
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+
+@Dao
 interface SiswaDao {
+    @Query("SELECT * from tblSiswa ORDER BY nama ASC")
+    fun getAllSiswa(): Flow<List<Siswa>>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(siswa: Siswa)
 }
